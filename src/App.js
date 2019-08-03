926349c60553dde12475ee2615d7eb9de0bf4240
Ian Logan <itlogan9@gmail.com>
2019-08-03 13:53:35 -0500

    Finish mobile form styling

--- src/App.js
+++ src/App.js
@@ -2,37 +2,55 @@ import React from "react";
 
 function App() {
   return (
-    <div className="px-8">
+    <div className="px-8 pt-4 pb-20">
       <header className="text-white text-center py-10">
-        <h1 className=" mt-4 text-4xl font-semibold leading-tight">
+        <h1 className=" mt-4 text-4xl font-bold leading-tight tracking-tight">
           Learn to code by watching others
         </h1>
-        <p className="mt-4 font-normal">
+        <p className="mt-5 font-normal">
           See how experienced developers solve problems in real-time. Watching
           scripted tutorials is great, but understanding how developers think is
           invaluable
         </p>
       </header>
-      <aside>
+      <aside className="text-center">
         <div className="bg-purple-800 text-white px-16 py-4 rounded-lg">
-          <p className="font-light">
+          <div className="font-light">
             <span className="font-semibold">Try it free 7 days</span> then
             $20/mo. thereafter
-          </p>
+          </div>
         </div>
-        <div className="mt-5 bg-gray-200 text-center p-4 rounded-lg shadow-inner">
-          <form className="w-full">
-            <input className="w-full" placeholder="First Name" />
-            <input className="w-full" placeholder="Last Name" />
-            <input className="w-full" placeholder="Email Address" />
-            <input className="w-full" placeholder="Password" />
-            <button className="w-full bg-green-400 uppercase text-white px-4 py-4">
-              Claim your free trial
-            </button>
+        <div className="mt-5 bg-gray-100 text-center p-6 rounded-lg shadow-md">
+          <form className="">
+            <div className="flex flex-col">
+              <input
+                className="bg-gray-100 border-2 border-gray-300 p-4 rounded-lg font-semibold"
+                placeholder="First Name"
+              />
+              <input
+                className="bg-gray-100 border-2 border-gray-300 p-4 rounded-lg font-semibold mt-4"
+                placeholder="Last Name"
+              />
+              <input
+                className="bg-gray-100 border-2 border-gray-300 p-4 rounded-lg font-semibold mt-4"
+                placeholder="Email Address"
+              />
+              <input
+                className="bg-gray-100 border-2 border-gray-300 p-4 rounded-lg font-semibold mt-4"
+                placeholder="Password"
+              />
+              <button className="bg-green-400 rounded-lg uppercase text-white font-semibold leading-relaxed px-4 py-4 mt-4">
+                Claim your free trial
+              </button>
+            </div>
           </form>
-          <small>
-            By clicking the button, you are agreeing to our Terms and Services
-          </small>
+          <div className="mt-1 px- leading-normal text-xs font-semibold text-gray-500 tracking-wide">
+            <small>
+              By clicking the button, you are agreeing to <br />
+              our{" "}
+              <span className="font-bold text-red-400">Terms and Services</span>
+            </small>
+          </div>
         </div>
       </aside>
     </div>
